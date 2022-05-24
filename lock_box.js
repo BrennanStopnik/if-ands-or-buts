40d3cef6f99bb8fa25fc4d29fee5d589c8ce7d84
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-05-24 15:19:10 -0400

    Started new project.
    Created files and brought on node_mods.

---
+++ lock_box.js
@@ -0,0 +1,20 @@
+/*
+You've been contracted to write the software for a lock box.
+
+Write a program called lock-box.js that stores the pin code to the lock box in a variable at the top of your program (i.e.: let pinCode = '1234';).
+
+After that, use prompt-sync to prompt the user for the passcode. If the user gets the passcode exactly correct, log a success message containing the word, "success". If not, log a failure message containing the word, "failure".
+
+Note: In this assignment, the node_modules folder containing the prompt-sync dependency does not already exist. To add it to this project, navigate to this repository using the command line then install it using the Node Package Manager with npm install prompt-sync.
+*/
+
+const prompt = require("prompt-sync")({sigint: true});
+
+let pass_prompt = Number(prompt("Please enter your passcode: "))
+let pass_code = 1134;
+
+if (pass_prompt === pass_code){
+    console.log("Success!");
+} else{
+    console.log("Failure!")
+}
